a4d4b59827dd00653f633c6f6a8c65aff2a022c2
unknown <zyadgado155@gmail.com>
2023-01-03 03:10:49 +0200

    second problem

---
+++ DetectCapital2_1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	bool detectCapitalUse(string word) {
+		int cnt = 0;
+		for (int i = 0; i < word.size(); i++)
+		{
+			if (isupper(word[i]))
+				cnt++;
+		}
+		if (cnt == 1 && isupper(word[0]) || word.size() == 1 || cnt == 0)
+			return true;
+		else if (cnt == word.size())
+			return true;
+		else return false;
+
+	}
+};
